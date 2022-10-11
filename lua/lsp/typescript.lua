7f32e3c100ed26aaadcd7b7a89e41e81f004e904
Fuzzy Yeti <fuzzyyeti@github.com>
2022-10-11 14:09:42 -0600

    add colors

--- lua/lsp/typescript.lua
+++ lua/lsp/typescript.lua
@@ -1,1 +1,2 @@
-require'lspconfig'.tsserver.setup {}
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require'lspconfig'.tsserver.setup { capabilities = capabilities}
